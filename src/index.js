62543818b3da9bb2eb7b4b6ddbb5adb37daf9a5e
Allwin <allwinraj2012@gmail.com>
2023-10-22 23:05:06 +0530

    Add the Google Analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
+
+//Google Analytics
+ReactGA.initialize('G-ZTWSFCW9PQ');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
