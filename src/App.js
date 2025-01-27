c26520f2faeb995250ebe76e0890be4e5991fe96
Allwin <allwinraj2012@gmail.com>
2025-01-28 00:00:02 +0530

    build file change

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import './App.css';
 import { NavBar } from './components/navbar';
 import { Banner } from './components/banner';
 import { Skills } from './components/skills';
-import { Projects } from './components/projects';
 import { Contact } from './components/contact';
-import { Footer } from './components/footer';
 import { Team } from './components/team';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
